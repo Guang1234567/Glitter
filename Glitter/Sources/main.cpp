012321bc051c71c8b1dbbcc4a172a34d1a8995b1
GuangGuang <lihansey163@163.com>
2020-03-10 16:42:00 +0800

    tmp

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -8,6 +8,13 @@
 // Standard Headers
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+const char* contentsOfShaderSource(const char* filePath);
+
+void framebuffer_size_changed(GLFWwindow* window, int width, int height);
 
 int main(int argc, char * argv[]) {
 
@@ -16,33 +23,179 @@ int main(int argc, char * argv[]) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+     #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+     #endif
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
     auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
 
     // Check for Valid Context
     if (mWindow == nullptr) {
-        fprintf(stderr, "Failed to Create OpenGL Context");
+        fprintf(stderr, "Failed to Create OpenGL Context\n");
+        glfwTerminate();
         return EXIT_FAILURE;
     }
 
     // Create Context and Load OpenGL Functions
     glfwMakeContextCurrent(mWindow);
-    gladLoadGL();
+    if (0 == gladLoadGL()) {
+      fprintf(stderr, "Failed to initialize GLAD\n");
+      glfwTerminate();
+      return EXIT_FAILURE;
+    }
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
+    glfwSetFramebufferSizeCallback(mWindow, framebuffer_size_changed);
+    
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const char* vertexShaderSource = contentsOfShaderSource("./vertexShaderSource.vert");
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success = 0;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (0 == success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        fprintf(stderr, "Failed to Create OpenGL Vertex Shader:\n%s\n", infoLog);
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    // fragment shader
+    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* fragmentShaderSource = contentsOfShaderSource("./fragmentShaderSource.frag");
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (0 == success) {
+      glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+      fprintf(stderr, "Failed to Create OpenGL Fragment Shader:\n%s\n",
+              infoLog);
+      glfwTerminate();
+      return EXIT_FAILURE;
+    }
+    // link shaders
+    int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        fprintf(stderr, "Failed to Link OpenGL Shaders:\n%s\n",
+                infoLog);
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    // delete shaders after linked shaders
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    /*
+     // 三角形
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // left
+         0.5f, -0.5f, 0.0f, // right
+         0.0f,  0.5f, 0.0f  // top
+    };
+    */
+    
+    // 矩形
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
+    };
+    
+    unsigned int indices[] = { // 注意索引从0开始!
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
+    
+    // VAO
+    unsigned int VAO = 0;
+    glGenVertexArrays(1, &VAO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+    
+    // VBO
+    unsigned int VBO = 0;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) /*bytes*/, vertices, GL_STATIC_DRAW);
+    // link VBO to Vertex shader
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+                          3 * sizeof(float) /*stride*/, (void *)0);
+    glEnableVertexAttribArray(0);
+    
+    //EBO
+    unsigned int EBO = 0;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                 GL_STATIC_DRAW);
+
+    // unbind EBO, VBO and VAO after linked VBO to Vertex shader
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    
     // Rendering Loop
     while (glfwWindowShouldClose(mWindow) == false) {
-        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(mWindow, true);
+        }
 
         // Background Fill Color
-        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+        // glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // bind VAO
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // bind EBO
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // unbind EBO
+        glBindVertexArray(0); // unbind VAO
 
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
         glfwPollEvents();
-    }   glfwTerminate();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
     return EXIT_SUCCESS;
+}
+
+
+const char* contentsOfShaderSource(const char* filePath) {
+    std::ifstream in(filePath);
+    std::string contents((std::istreambuf_iterator<char>(in)),
+                         std::istreambuf_iterator<char>());
+    return contents.c_str();
+}
+
+void framebuffer_size_changed(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
